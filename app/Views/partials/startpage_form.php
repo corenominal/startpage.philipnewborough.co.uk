ba32a27febddb613c7c3ba68319723c4e9ca277a
Philip Newborough <corenominal@corenominal.org>
2026-04-25 07:47:09 +0100

    fix: Change button style for submit action to primary

--- app/Views/partials/startpage_form.php
+++ app/Views/partials/startpage_form.php
@@ -2,6 +2,6 @@
     <div class="input-group input-group-lg">
         <span id="q-icon" class="input-group-text"><i class="bi bi-chevron-right"></i></span>
         <input id="q" name="q" type="text" class="form-control" placeholder="" autocomplete="off" required>
-        <button type="submit" class="btn btn-dark"><i class="bi bi-arrow-return-left"></i></button>
+        <button type="submit" class="btn btn-primary"><i class="bi bi-arrow-return-left"></i></button>
     </div>
 </form>
